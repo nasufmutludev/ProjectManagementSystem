d069cbbfcefe58afa247c03e15de943894c7aede
Nasuf MUTLU <nasufmutlu@hotmail.com>
2022-09-11 20:23:54 +0300

    .

--- DataAccess/Concrete/EntityFramework/ProjectManagementContext.cs
+++ DataAccess/Concrete/EntityFramework/ProjectManagementContext.cs
@@ -8,9 +8,6 @@ namespace DataAccess.Concrete.EntityFramework
         public DbSet<Department> Departments { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<Team> Teams { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<UserRole> UserRoles { get; set; }
         public DbSet<TaskList> Tasks { get; set; }
     }
 }
